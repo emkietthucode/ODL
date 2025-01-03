1c87163fdb18b3751f933ee148c615ccb9456387
hphuc0324 <sippermana3@gmail.com>
2025-01-03 16:51:21 +0700

    update footer, navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,38 @@
-const Home = () => {
-   return (
-    <main className="flex-grow flex flex-col justify-evenly text-left m-10 p-10 bg-white">
-      <h1 className="text-3xl font-bold text-purple">
-        ODL - Chinh phục tay lái, vượt mọi giới hạn!
-      </h1>
-      <p className="text-gray-700 mb-8 max-w-xl">
-        ODL mang đến giải pháp ôn thi bằng lái xe nhanh chóng và tiện lợi, giúp bạn học mọi lúc, mọi nơi. Hệ thống bài thi sát thực tế và hỗ trợ đa quốc gia, ODL đồng hành cùng bạn trên hành trình chinh phục kỳ thi lái xe dễ dàng nhất.
-      </p>
-      <button className="bg-purple font-bold text-2xl text-white px-6 py-3 rounded-full shadow-lg hover:bg-purple-600 w-fit">
-        Luyện thi ngay
-      </button>
-      <div className="mt-12">
-        <img src="car-illustration.png" alt="Driving School" className="w-full max-w-md" />
-      </div>
-    </main>     
-      
-  );
-};
+import ScrollToTopButton from '@/components/scroll-to-top-button'
+import { Button } from '../components/ui/button'
 
-export default Home;
+const Home = () => {
+  return (
+    <main className="text-left m-10 p-10 bg-white">
+      <div>
+        <div className="max-w-[1200px] mx-auto">
+          <h1 className="text-3xl font-bold text-purple w-full text-[40px]">
+            ODL - Chinh phục tay lái, vượt mọi giới hạn!
+          </h1>
+          <p className="text-gray-700 mt-12 text-[28px] max-w-[760px]">
+            ODL mang đến giải pháp ôn thi bằng lái xe nhanh chóng và tiện lợi,
+            giúp bạn học mọi lúc, mọi nơi. Hệ thống bài thi sát thực tế và hỗ
+            trợ đa quốc gia, ODL đồng hành cùng bạn trên hành trình chinh phục
+            kỳ thi lái xe dễ dàng nhất.
+          </p>
+
+          <Button
+            variant="main"
+            size="auto"
+            className="text-4xl mt-20 px-6 py-3 rounded-full shadow-lg w-fit"
+          >
+            Luyện thi ngay
+          </Button>
+        </div>
+      </div>
+
+      <div className="mt-12">
+        {/* <img src="car-illustration.png" alt="Driving School" className="w-full max-w-md" /> */}
+      </div>
+
+      <ScrollToTopButton />
+    </main>
+  )
+}
+
+export default Home
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,36 @@
-
 const NavBar = () => {
-    return (
-      <div className="flex justify-around items-center p-4">
-        <div className="text-gray-500">Logo</div>
-        <nav className="flex justify-center items-center space-x-4 text-gray-500">
-            <a href="#" className="hover:text-purple">Trang chủ</a>
-            <a href="#" className="hover:text-purple">Thi thử</a>
-            <a href="#" className="hover:text-purple">Luyện thi</a>
-            <a href="#" className="hover:text-purple">Liên hệ</a>
-            <a href="#" className="text-purple font-bold underline">ĐĂNG KÝ</a>
-            <a href="#" className="bg-purple font-bold text-white px-4 py-2 rounded-full">ĐĂNG NHẬP</a>
+  return (
+    <div className="flex justify-around items-center p-4">
+      <div className="text-gray-500">Logo</div>
+      <div className="flex gap-10">
+        <nav className="flex justify-center items-center gap-6 text-gray-500">
+          <a href="#" className="hover:text-purple">
+            Trang chủ
+          </a>
+          <a href="#" className="hover:text-purple">
+            Thi thử
+          </a>
+          <a href="#" className="hover:text-purple">
+            Luyện thi
+          </a>
+          <a href="#" className="hover:text-purple">
+            Liên hệ
+          </a>
+        </nav>
+        <nav className="flex gap-3 items-center">
+          <a href="#" className="text-purple font-bold underline">
+            ĐĂNG KÝ
+          </a>
+          <a
+            href="#"
+            className="bg-purple font-bold text-white px-4 py-2 rounded-full"
+          >
+            ĐĂNG NHẬP
+          </a>
         </nav>
       </div>
-    );
+    </div>
+  )
 }
- 
-export default NavBar;
+
+export default NavBar
