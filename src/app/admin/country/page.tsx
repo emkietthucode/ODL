11b68f45777785d714adbd5e3bdb69143591b85c
Dang Manh Phuc <dangmanhphuc2003@gmail.com>
2025-01-05 17:05:16 -0800

    route for country management

--- src/app/admin/country/page.tsx
+++ src/app/admin/country/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import useInsertCountryModal from "../../../hooks/useInsertCountryModal";
+import useInsertCountryModal from "../../../../hooks/useInsertCountryModal";
 
 const CountryDashboard = () => {
     const insertCountryModal = useInsertCountryModal();
