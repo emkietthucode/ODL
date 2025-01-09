cef297706edc00ff58bb954fe8059db71b678007
Dang Manh Phuc <dangmanhphuc2003@gmail.com>
2025-01-09 12:50:15 +0700

    fix(component): modal blurness

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -19,79 +19,69 @@ const Modal: React.FC<ModalProps> = ({
   return (
     <Dialog.Root open={isOpen} defaultOpen={isOpen} onOpenChange={onChange}>
       <Dialog.Portal>
-        <Dialog.Overlay
-          className="
-                    backdrop-blur-sm
-                    fixed
-                    inset-0
-                "
-        />
-        <Dialog.Content
-          className="
-                    fixed
-                    drop-shadow-md
-                    border
-                    border-purple
-                    top-[50%]
-                    left-[50%]
-                    max-h-full
-                    h-full
-                    md:h-auto
-                    md:max-h-[85vh]
-                    w-full
-                    md:w-[90vw]
-                    md:max-w-[450px]
-                    translate-x-[-50%]
-                    translate-y-[-50%]
-                    rounded-md
-                    bg-white
-                    p-[25px]
-                    focus:outline-none
-                "
-        >
-          <Dialog.Title
+        <Dialog.Overlay className="backdrop-blur-sm fixed inset-0 flex items-center justify-center">
+          <Dialog.Content
             className="
-                        text-xl
-                        text-center
-                        font-bold
-                        mb-4
-                    "
+              drop-shadow-md
+              border
+              border-purple
+              max-h-full
+              h-full
+              md:h-auto
+              md:max-h-[85vh]
+              w-full
+              md:w-[90vw]
+              md:max-w-[450px]
+              rounded-md
+              bg-white
+              p-[25px]
+              focus:outline-none
+            "
           >
-            {title}
-          </Dialog.Title>
-          <Dialog.Description
-            className="
-                        mb-5
-                        text-sm
-                        leading-normal
-                        text-center
-                    "
-          >
-            {description}
-          </Dialog.Description>
-          <div>{children}</div>
-          <Dialog.Close asChild>
-            <button
+            <Dialog.Title
               className="
-                            text-neutral-400
-                            hover:text-black
-                            absolute
-                            top-[10px]
-                            right-[10px]
-                            inline-flex
-                            h-[25px]
-                            w-[25px]
-                            appearance-none
-                            items-center
-                            justify-center
-                            rounded-full
-                            focus:outline-none
-                        "
+                text-xl
+                text-center
+                font-bold
+                mb-4
+              "
             >
-              <IoMdClose />
-            </button>
-          </Dialog.Close>
-        </Dialog.Content>
+              {title}
+            </Dialog.Title>
+            <Dialog.Description
+              className="
+                mb-5
+                text-sm
+                leading-normal
+                text-center
+              "
+            >
+              {description}
+            </Dialog.Description>
+            <div>{children}</div>
+            <Dialog.Close asChild>
+              <button
+                className="
+                  text-neutral-400
+                  hover:text-black
+                  absolute
+                  top-[10px]
+                  right-[10px]
+                  inline-flex
+                  h-[25px]
+                  w-[25px]
+                  appearance-none
+                  items-center
+                  justify-center
+                  rounded-full
+                  focus:outline-none
+                "
+              >
+                <IoMdClose />
+              </button>
+            </Dialog.Close>
+          </Dialog.Content>
+        </Dialog.Overlay>
       </Dialog.Portal>
     </Dialog.Root>
   )
