d12c551c617f6597ecc61b5c0c70da9252fc7aac
Dang Manh Phuc <dangmanhphuc2003@gmail.com>
2025-01-10 10:46:46 +0700

    feat: add navigation bar

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,14 +1,18 @@
+import NavBar from '@/components/navbar'
 import SideBar from '@/components/sidebar'
 import ModalProvider from '@/providers/modal-provider'
 import ToasterProvider from '@/providers/toaster-provider'
 
 const AdminDashboard = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex min-h-screen">
-      <ModalProvider />
-      <SideBar />
-      <ToasterProvider />
-      <div className="grow border-l-2 border-light-purple">{children}</div>
+    <div className="flex flex-col min-h-screen">
+      <NavBar />
+      <div className="flex flex-row flex-1">
+        <ModalProvider />
+        <SideBar />
+        <ToasterProvider />
+        <div className="grow border-l-2 border-light-purple">{children}</div>
+      </div>
     </div>
   )
 }
