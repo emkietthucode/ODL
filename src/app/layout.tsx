a00b9e88fff6892b4cbc360bf7f2bf8b7dab514c
Dang Manh Phuc <dangmanhphuc2003@gmail.com>
2025-01-22 16:45:33 +0700

    fix: apply Montserrat font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import './globals.css'
 import { AuthProvider } from '@/contexts/AuthContext'
 
 const montserrat = Montserrat({
-  variable: '--font-montserrat',
   subsets: ['latin'],
 })
 
@@ -20,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${montserrat.variable} antialiased`}>
+      <body className={`${montserrat.className} antialiased`}>
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
