3309e7216f0399f9100c8ce8b32697fa343dd710
Dang Manh Phuc <dangmanhphuc2003@gmail.com>
2025-04-02 07:23:39 +0700

    fix: make NavBar sticky for improved navigation accessibility

--- src/app/(common)/layout.tsx
+++ src/app/(common)/layout.tsx
@@ -4,7 +4,9 @@ import Footer from '@/components/footer'
 function CommonLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <NavBar />
+      <div className="sticky top-0 z-50 bg-white">
+        <NavBar />
+      </div>
       {children}
       <Footer />
     </div>
