eafcdf144b29fbb9bd0b3d2d30e3417e42629af6
hphuc0324 <sippermana3@gmail.com>
2025-05-07 00:39:42 +0700

    fix: user loading

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -3,17 +3,20 @@
 import { createClient } from '@/utils/supabase/client'
 import { User } from '@supabase/supabase-js'
 import { createContext, useEffect, useState } from 'react'
+import { NguoiDung } from '@/types/types'
 
 interface AuthContextType {
   user: User | null
+  userData: NguoiDung | null
   loading: boolean
   setUser: (user: User | null) => void
 }
 
-export const AuthContext = createContext<AuthContextType | undefined>(undefined)
+export const AuthContext = createContext<AuthContextType | null>(null)
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<User | null>(null)
+  const [userData, setUserData] = useState<NguoiDung | null>(null)
   const [loading, setLoading] = useState(true)
 
   const [supabase] = useState(() => createClient())
@@ -28,16 +31,55 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 
       setUser(session?.user || null)
 
-      setLoading(false)
+      if (session?.user) {
+        try {
+          const { data, error } = await supabase
+            .from('nguoi_dung')
+            .select('*')
+            .eq('id', session.user.id)
+            .single()
+
+          if (!error && data) {
+            setUserData(data)
+          }
+        } catch (error) {
+        } finally {
+          setLoading(false)
+        }
+      } else {
+        setUserData(null)
+        setLoading(false)
+      }
     }
 
     fetchUser()
 
     const {
       data: { subscription },
-    } = supabase.auth.onAuthStateChange((event, session) => {
+    } = supabase.auth.onAuthStateChange(async (event, session) => {
       console.log(event)
       setUser(session?.user || null)
+      setLoading(false)
+
+      if (session?.user) {
+        try {
+          const { data, error } = await supabase
+            .from('nguoi_dung')
+            .select('*')
+            .eq('id', session.user.id)
+            .single()
+
+          if (!error && data) {
+            setUserData(data)
+          }
+        } catch (error) {
+        } finally {
+          setLoading(false)
+        }
+      } else {
+        setUserData(null)
+        setLoading(false)
+      }
     })
 
     return () => {
@@ -48,7 +90,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   console.log(user)
 
   return (
-    <AuthContext.Provider value={{ user, loading, setUser }}>
+    <AuthContext.Provider value={{ user, userData, loading, setUser }}>
       {children}
     </AuthContext.Provider>
   )
